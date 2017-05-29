88ec1e7af6ce6770e92d9d1eae5aa9b868b73512
Fabrizio Giuliano <fabrizio.giuliano@dieet.unipa.it>
2017-05-29 10:39:34 +0200

    update run_enb.sh, run_ue.sh, add set_lte_tx_pattern.py

--- code/LTE/run_enb.sh
+++ code/LTE/run_enb.sh
@@ -1,5 +1,9 @@
 #USRP_DEV="serial=30AD308"
 #USRP_DEV="serial=30AD345"
+if [ $# -lt 4 ]; then
+	echo "usage:sh run_enb.sh <SERIAL-USRP> <TX_PATTERN> <PRB> <FREQ>"
+	exit
+fi
 USRP_DEV="serial=$1"
 #tx_pattern="1,1,1,1,1,1,0,0,0,0"
 tx_pattern=$2
@@ -7,7 +11,4 @@ PRB=$3
 gain=90
 freq=$4 #5000e6
 
-#./srsLTE/build/srslte/examples/pdsch_enodeb -a $USRP_DEV -f 2437e6 -g 90 -l 0.7 -m 28 -u 2000 -b f -w $tx_pattern
-
 ./srsLTE/build/srslte/examples/pdsch_enodeb -a $USRP_DEV -f $freq -p $PRB -g $gain -l 0.7 -m 28 -u 2000 -b f -w $tx_pattern
-#./srsLTE/build/srslte/examples/pdsch_enodeb -a $USRP_DEV -f 2462e6 -p $PRB -g 90 -l 0.7 -m 28 -u 2000 -b f -w $tx_pattern
--- code/LTE/run_ue.sh
+++ code/LTE/run_ue.sh
@@ -1,9 +1,12 @@
 USRP_DEV="serial=30AD308"
 USRP_DEV="serial=30AD345"
+
+if [ $# -lt 3 ]; then
+	echo "sh run_ue.sh <SERIAL> <PORT> <FREQ>"
+	exit
+fi
+
 USRP_DEV="serial=$1"
 port=$2
-#while [ 1 ]; do ./srsLTE/build/srslte/examples/pdsch_ue -a $USRP_DEV -f 2437e6 -r 1234 -u 2001 -U 127.0.0.1; sleep 1; done
-#while [ 1 ]; do ./srsLTE/build/srslte/examples/pdsch_ue -f 2437e6 -r 1234 -s 2001 -S 127.0.0.1; sleep 1; done
-
-#./srsLTE/build/srslte/examples/pdsch_ue -a $USRP_DEV -f 2437e6 -r 1234 -u 2001 -U 127.0.0.1
-./srsLTE/build/srslte/examples/pdsch_ue -a $USRP_DEV -f 2437e6 -r 1234 -u $port -U 127.0.0.1
+freq=$3
+./srsLTE/build/srslte/examples/pdsch_ue -a $USRP_DEV -f $freq -r 1234 -u $port -U 127.0.0.1
